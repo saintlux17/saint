aef8e232344e7d7f1c685fc6e93d4d850efb3a2f
saintlux17 <77097045+saintlux17@users.noreply.github.com>
2022-09-02 05:31:32 +0900

    Update mul_36.v

--- mac_core_36/mul_36.v
+++ mac_core_36/mul_36.v
@@ -11,7 +11,9 @@ module mul_36
     input[8*N-1:0] in_2,
     input[8*N-1:0] in_3,
     output[16*N-1:0] out1,
-    output[16*N-1:0] out2
+    output[16*N-1:0] out2,
+    output vld_o1,
+    output vld_o2
     );
 
 genvar i;
@@ -28,5 +30,13 @@ for (i = 1; i <= N; i=i+1) begin
     end
 endgenerate
 
+always@(*)begin
+    if(out1) begin
+        vld_o1 <= 1'd1;
+        end
+    if(out2) begin
+        vld_o2 <= 1'd1;
+        end
+    end
 
 endmodule
